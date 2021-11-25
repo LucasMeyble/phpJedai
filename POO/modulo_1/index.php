c418ba2b8a2548750dedb0e1eeb4df46e51cdb88
lucas Meyble <lucasmeyble123@gmail.com>
2021-11-25 00:37:20 -0300

    introdução a poo

---
+++ POO/modulo_1/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+  class Pessoa {
+    private $name = 'lucas';
+    private $idade = '21';
+    private $peso = '80kg';
+
+    public function crescer($nome){
+      echo 'estou crescendo';
+    }
+
+    public function comer(){
+      echo 'esotu comendo';
+    }
+  }
+
+  $pessoa = new Pessoa;
+
+  $pessoa->crescer();
+
+?>

0fae474131a44d307b8f59ccebadbe51b9e9299d
lucasMeyble <lucasmeyble123@gmail.com>
2021-11-29 21:39:59 -0300

    public, static e private

---
+++ POO/modulo_1/Exemplo.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class Exemplo
+{
+    private $var1;
+    public $var2;
+    static $var3 = 'vaariavel estatica';
+    
+    static function metodoEstatico(){
+        echo 'esse metodo é estatico';
+    }
+}
--- POO/modulo_1/index.php
+++ POO/modulo_1/index.php
@@ -1,21 +1,39 @@
 <?php 
 
-  class Pessoa {
-    private $name = 'lucas';
-    private $idade = '21';
-    private $peso = '80kg';
+  // class Pessoa {
+  //   private $name = 'lucas';
+  //   private $idade = '21';
+  //   private $peso = '80kg';
 
-    public function crescer($nome){
-      echo 'estou crescendo';
-    }
+  //   public function crescer($nome){
+  //     echo 'estou crescendo';
+  //   }
 
-    public function comer(){
-      echo 'esotu comendo';
-    }
-  }
+  //   public function comer(){
+  //     echo 'esotu comendo';
+  //   }
+  // }
 
-  $pessoa = new Pessoa;
+  // $pessoa = new Pessoa;
 
-  $pessoa->crescer('teste');
+  // $pessoa->crescer('teste');
+
+  include('Exemplo.class.php');
+
+  // $exemplo = new Exemplo();
+
+  // $exemplo->var2 = 'lucas meyble';
+  
+  // echo $exemplo->var2;
+  // echo '<br>';
+  // $exemplo2 = new Exemplo();
+  // $exemplo2->var2 = 'joao';
+  // echo $exemplo2->var2;
+
+  echo Exemplo::$var3;
+  echo '<br>';
+  echo Exemplo::$var3 = 'teste';
+  echo '<br>';
+  Exemplo::metodoEstatico();
 
 ?>
